d5f87da39fc34129586b40f8b22c92cb84900211
Bwalya <gabrielmaele1@yahoo.com>
2023-04-24 20:03:33 -0500

    Testing checkout-session/Still needs work tho
    Set up the webhook, added product on stripe, created checkout session, new restricted API keys

--- main/public/server.js
+++ main/public/server.js
@@ -3,10 +3,31 @@ const express = require('express');
 const admin = require('firebase-admin');
 const bcrypt = require('bcrypt');
 const path = require('path');
+const { pathToFileURL } = require('url');
+const stripe = require('stripe')('sk_test_51MzkXaEJsQttsu4Fyt2KWQlRsfkjXH0s3x4O9MIYQTrG57zSoFlPkaaOXaDOui6H4zD93inDiJltFFdHYKTThZng00340CWJ3J'); // secret key
+
+// checkout session
+app.post(path.join(__dirname,"/checkout-session"), async (req, res) => {
+    const session = await stripe.checkout.sessions.create({
+        line_items: [
+            {
+            amount: 'price_1N0XkjEJsQttsu4FFmP5PYDi',
+            quantity: 1,
+            }
+        ],
+        node: 'payment',
+        success_url: 'https://csce5560-project.web.app/',
+        cancel_url: 'https://csce5560-project.web.app/',
+    });
+    
+// Redirect to the URL returned on the Checkout Session.
+    res.redirect(303, session.url);
+   
+});
 
 //intializing expess.js
 
-const app = ecpress();
+const app = express();
 
 //routes
 //home route
