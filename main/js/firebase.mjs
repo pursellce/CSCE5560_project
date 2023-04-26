c2467dbf2977aaeba9d78942d95eb632f45d652c
pursellce <pursellce@gmail.com>
2023-04-26 08:54:06 -0500

    fixed typo in import name

--- main/js/firebase.mjs
+++ main/js/firebase.mjs
@@ -2,7 +2,7 @@
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.20.0/firebase-app.js';
 import { getFirestore, collection, getDocs } from 'https://www.gstatic.com/firebasejs/9.20.0/firebase-firestore.js';
 import { getAuth, createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword } from 'https://www.gstatic.com/firebasejs/9.20.0/firebase-auth.js';
-import {initializeAppCheck, ReCaptchaV3Provider} from 'https://www.gstatic.com/firebasejs/9.20.0/firebase-app-check';
+import {initializeAppCheck, ReCaptchaV3Provider} from 'https://www.gstatic.com/firebasejs/9.20.0/firebase-app-check.js';
 
 //Configuration
 const firebaseConfig = {
