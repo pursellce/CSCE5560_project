a23af02d0d6daf32f2f6d23bf9d07b8bd01f3000
BDeLuia <bdeluia@gmail.com>
2023-04-30 00:57:58 -0500

    Did you know every js files needs to initialize the fireabse app in order to use fireabse functions?
    Because it does. yea. added to index.js file :)

--- main/js/index.js
+++ main/js/index.js
@@ -1,3 +1,19 @@
+import { initializeApp } from 'firebase/app'
+//Configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyBrgPio0COrd8AiMDiBb7sWsUx_Xl7Eip0",
+    authDomain: "csce5560-project.firebaseapp.com",
+    databaseURL: "https://csce5560-project-default-rtdb.firebaseio.com",
+    projectId: "csce5560-project",
+    storageBucket: "csce5560-project.appspot.com",
+    messagingSenderId: "53120338249",
+    appId: "1:53120338249:web:4839a3e417b0345ad6f298",
+    measurementId: "G-8JE4QWELZJ"
+  };
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
 import { getAuth, onAuthStateChanged } from "firebase/auth";
 
 const auth = getAuth();
@@ -5,7 +21,6 @@ onAuthStateChanged(auth, (user) => {
   if (user) {
     document.getElementById('login').style.visibility = 'hidden';
     document.getElementById('signup').style.visibility = 'hidden';
-    alert("You are logged in!")
   } else {
     document.getElementById('login').style.visibility = 'visible';
     document.getElementById('signup').style.visibility = 'visible';
