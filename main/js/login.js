0d7a1a8b12a307b4ba5b2f85caacd167f50b0065
pursellce <pursellce@gmail.com>
2023-05-01 13:25:41 -0500

    More login attempts

--- main/js/login.js
+++ main/js/login.js
@@ -35,57 +35,62 @@ onAuthStateChanged(auth, (user) => {
 
 //LOGIN Function
 var loginForm = document.getElementById("loginFormID");
+//User enters email and password and hits submit button
 loginForm.addEventListener("submit", ()=> {
   var email = document.getElementById("loginEmailID").value;
   var password = document.getElementById("loginPasswordID").value;
-  //login user with email and password
+  //Begin logging in user with email and password
   signInWithEmailAndPassword(auth, email, password)
-    //prevent login by catching error generated when an account has mfa enabled
+    //Catch error generated when account has mfa enabled (all users)
     .catch(function(error){
       if(error.code == 'auth/multi-factor-auth-required'){
-      //hide email and password input boxes, display mfa input box
+        //To DO: if user is able to sign in without mfa delete their account and 
+        //instruct to create new account with phone
+
+        //hide email and password input boxes/submit button, display mfa input box/submit button
         var emailPassInfo = document.getElementById("emailPassInfoID");
-        var mfa = document.getElementById("mfaID");
         emailPassInfo.style.display = "none";
-        mfa.style.diplay = "block";
+        var mfa = document.getElementById("mfaID");
+        mfa.style.diplay = "inline-block";
+
         //begin mfa login steps
         const resolver = getMultiFactorResolver(auth, error);
-        console.log(selectedIndex);
-        const phoneInfoOptions ={
-          multiFactorHint: resolver.hints[selectedIndex],
-          session: resolver.session
-        }
-        //send SMS code
-        const recaptchaVerifier = new RecaptchaVerifier(document.getElementById("recaptcha-container"), {
-          size: 'invisible',
-          callback: () =>{
-          //send SMS verification code
-            const phoneAuthProvider = new PhoneAuthProvider(auth);
-            phoneAuthProvider.verifyPhoneNumber(phoneInfoOptions, recaptchaVerifier)
-              .then(function(verificationId){
-                  //get SMS Verifcation Code from user
-                document.getElementById("mfaCodeSubmit").onclick = function(){
-                  var verificationCode = document.getElementById("otpID").value;
-                  const cred = PhoneAuthProvider.credential(
-                    verificationId, verificationCode);
-                  const multiFactorAssertion =
-                   PhoneMultiFactorGenerator.assertion(cred);
-                  //complete signin
-                  console.log("Completing MFA login");
-                  return resolver.resolveSignIn(multiFactorAssertion)
-                    .then(()=>{
-                      console.log("MFA Login Sucessful!");
-                      window.location.href = "index.html";
-                    })
-                    .catch((error) => {
-                      console.error('Error resolving mfa login')
-                    })
-                }
-              })
+        if(resolver.hints[selectedIndex].factorId === PhoneMultiFactorGenerator.FACTOR_ID){
+          const phoneInfoOptions ={
+            multiFactorHint: resolver.hints[selectedIndex],
+            session: resolver.session
+          };
+          //send SMS code
+          const recaptchaVerifier = new RecaptchaVerifier(document.getElementById("recaptcha-container"), {
+            size: 'invisible',
+            callback: () =>{
+            //send SMS verification code
+              const phoneAuthProvider = new PhoneAuthProvider(auth);
+              phoneAuthProvider.verifyPhoneNumber(phoneInfoOptions, recaptchaVerifier)
+                .then(function(verificationId){
+                    //get SMS Verifcation Code from user
+                  document.getElementById("mfaCodeSubmit").onclick = function(){
+                    var verificationCode = document.getElementById("otpID").value;
+                    const cred = PhoneAuthProvider.credential(
+                      verificationId, verificationCode);
+                    const multiFactorAssertion =
+                    PhoneMultiFactorGenerator.assertion(cred);
+                    //complete signin
+                    console.log("Completing MFA login");
+                    return resolver.resolveSignIn(multiFactorAssertion)
+                      .then(()=>{
+                        console.log("MFA Login Sucessful!");
+                        window.location.href = "index.html";
+                      })
+                      .catch((error) => {
+                        console.error('Error resolving mfa login')
+                      })
+                  }
+                })
+              },
             },
-          },
-        auth);
-      }
+          auth);
+        }
       else if(error.code == 'auth/wrong-password'){
         loginForm.reset();
         alert("Password is incorrect, please try again.")
@@ -96,11 +101,11 @@ loginForm.addEventListener("submit", ()=> {
         const errorMessage = error.message;
         console.log(errorCode, errorMessage);
         alert("Something went wrong...please try again");
-
-
       }
-    })
+    }
 })
+})
+
 
 
 
